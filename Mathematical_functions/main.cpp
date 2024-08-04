cf801ccea903f7b7f7322cc786d62cd7e362efec
Alexander Nesterov <alex18nest@icloud.com>
2024-08-04 08:25:46 -0300

    Initial Commit

---
+++ Mathematical_functions/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Mathematical_functions
+//
+//  Created by Александр Нестеров on 8/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
