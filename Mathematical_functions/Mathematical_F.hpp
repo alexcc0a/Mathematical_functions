56e32ec654fd0d61271106de67921ed47cd072cd
Alexander Nesterov <alex18nest@icloud.com>
2024-08-04 09:38:18 -0300

    [+] Methematical functions

---
+++ Mathematical_functions/Mathematical_F.hpp
@@ -0,0 +1,19 @@
+//
+//  Mathematical_F.hpp
+//  Mathematical_functions
+//
+//  Created by Александр Нестеров on 8/4/24.
+//
+
+#ifndef Mathematical_F_hpp
+#define Mathematical_F_hpp
+
+#include <stdio.h>
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+double power(double a, double b);
+
+#endif //Mathematical_F_hpp
--- Mathematical_functions/main.cpp
+++ Mathematical_functions/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+#include "Mathematical_F.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    double num1, num2;
+    char operation;
+    std::cout << "Числа, необходимо вводить через пробел\n";
+    std::cout << "Введите первое число: ";
+    std::cin >> num1;
+    std::cout << "Введите второе число: ";
+    std::cin >> num2;
+    
+    std::cout << "Выберите операцию (+, -, *, /, ^): ";
+    std::cin >> operation;
+    
+    try {
+        double result;
+        switch(operation) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+            case '/':
+                result = divide(num1, num2);
+                break;
+            case '^':
+                result = power(num1, num2);
+                break;
+            default:
+                std::cout << "Неверная операция" << std::endl;
+                return 1;
+        }
+        
+        std::cout << "Результат: " << result << std::endl;
+    } catch (const std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
